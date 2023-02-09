edd18cd36dd8641b589318214ea415d5b82614a8
bstrat <benstratton100@gmail.com>
2023-02-09 15:41:45 -0600

    started navbar for mobile

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ window.addEventListener(`scroll`, function(){
     let header = document.querySelector(`header`)
     header.classList.toggle(`sticky`, window.scrollY > 0)
 })
+// Hamburger
+const hamburger = document.querySelector(`.hamburger`)
+const navMenu = document.querySelector(`.nav-bar`)
+hamburger.addEventListener(`click`, function(){
+    hamburger.classList.toggle(`active`)
+    navMenu.classList.toggle(`active`)
+})
+document.querySelectorAll(`.nav-link`).forEach(n => n.addEventListener(`click`, function(){
+    hamburger.classList.remove(`active`)
+    navMenu.classList.remove(`active`)
+}))
