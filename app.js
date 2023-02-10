f28a19d73ee3bd0e0ba515dc99ab83dd95e343a3
bstrat <benstratton100@gmail.com>
2023-02-09 18:21:25 -0600

    added .gitignore and finished media query for nav bar

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ window.addEventListener(`scroll`, function(){
     let header = document.querySelector(`header`)
     header.classList.toggle(`sticky`, window.scrollY > 0)
 })
-// Hamburger
-const hamburger = document.querySelector(`.hamburger`)
-const navMenu = document.querySelector(`.nav-bar`)
-hamburger.addEventListener(`click`, function(){
-    hamburger.classList.toggle(`active`)
-    navMenu.classList.toggle(`active`)
-})
-document.querySelectorAll(`.nav-link`).forEach(n => n.addEventListener(`click`, function(){
-    hamburger.classList.remove(`active`)
-    navMenu.classList.remove(`active`)
-}))
+// Hamburger Nav Bar
+function toggleMenu() {
+    const menuToggle = document.querySelector(`.menuToggle`)
+    const navigation = document.querySelector(`.navigation`)
+    menuToggle.classList.toggle(`active`)
+    navigation.classList.toggle(`active`)
+}
